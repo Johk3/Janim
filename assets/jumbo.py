3c0180259d1a39d0827ec64f2154bf22f366416a
Johannes Leppäkorpi <user3610@protonmail.com>
2020-10-15 22:49:58 +0300

    Look functionality added to Jumbo

--- assets/jumbo.py
+++ assets/jumbo.py
@@ -2,17 +2,25 @@ from manimlib.imports import *
 # from math import sin, cos, tan, pi
 
 class Character(Scene):
-    @staticmethod
-    def create_jumbo():
-        circle = Circle(radius=2, color=PINK)
-        left_eye = Circle(radius=.5, color=GREEN)
-        left_inner = Circle(radius=.1, color=WHITE, fill_color=WHITE, fill_opacity=1)
-        right_eye = Circle(radius=.5, color=GREEN)
-        right_inner = Circle(radius=.1, color=WHITE, fill_color=WHITE, fill_opacity=1)
-        left_eye.shift(LEFT)
-        left_inner.shift(LEFT)
-        right_eye.shift(RIGHT)
-        right_inner.shift(RIGHT)
+    def __init__(self, radius=2, color_top=PINK, color_outer_eye=GREEN, color_inner_eye=WHITE):
+        self.circle = Circle(radius=radius, color=color_top)
+        self.left_eye = Circle(radius=radius*0.25, color=color_outer_eye)
+        self.left_inner = Circle(radius=radius*0.05, color=color_inner_eye, fill_color=color_inner_eye, fill_opacity=1)
+        self.right_eye = Circle(radius=radius*0.25, color=color_outer_eye)
+        self.right_inner = Circle(radius=radius*0.05, color=color_inner_eye, fill_color=color_inner_eye, fill_opacity=1)
+        self.left_eye.shift(LEFT*(radius*0.5))
+        self.left_inner.shift(LEFT*(radius*0.5))
+        self.right_eye.shift(RIGHT*(radius*0.5))
+        self.right_inner.shift(RIGHT*(radius*0.5))
+        self.radius = radius
 
-        Jumbo = VGroup(*[circle, left_eye, right_eye, left_inner, right_inner])
+    def create_jumbo(self):
+        Jumbo = VGroup(*[self.circle, self.left_eye, self.right_eye, self.left_inner, self.right_inner])
+        return Jumbo
+
+    def look(self, target):
+        Jumbo = VGroup(*[self.circle, self.left_eye, self.right_eye, self.left_inner, self.right_inner])
+        for coord in target:
+            self.left_inner.shift((self.radius*0.5)*0.1*coord)
+            self.right_inner.shift((self.radius*0.5)*0.1*coord)
         return Jumbo
--- testbench.py
+++ testbench.py
@@ -3,5 +3,10 @@ from assets.jumbo import Character
 
 class Shapes(Scene):
     def construct(self):
-        jumbo = Character.create_jumbo()
-        self.play(ShowCreation(jumbo))
+        jumbo = Character(radius=0.8)
+        target = np.array([DOWN, LEFT])
+        jumbo_s = jumbo.create_jumbo()
+        self.play(ShowCreation(jumbo_s))
+        self.wait(2)
+        self.play(ReplacementTransform(jumbo_s, jumbo.look(target)))
+        self.wait(2)
